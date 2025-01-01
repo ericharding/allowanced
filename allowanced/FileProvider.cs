4855b0a363c66159bf7a6bddc2b6aa0dbaaa226e
Eric Harding <eric@digitalsorcery.net>
2025-01-01 11:45:35 -0500

    Add FileProvider and move content files over

---
+++ allowanced/FileProvider.cs
@@ -0,0 +1,70 @@
+using System.Collections.Immutable;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.FileProviders;
+using Scriban;
+
+namespace allowanced
+{
+    interface IFileProvider
+    {
+        string getFileContents(string fileName);
+        Scriban.Template getTemplate(string fileName);
+    }
+
+    public record LocalFileProvider(string root) : IFileProvider
+    {
+        public string getFileContents(string fileName)
+        {
+            return File.ReadAllText(Path.Combine(root, fileName));
+        }
+
+        public Template getTemplate(string fileName)
+        {
+            var content = getFileContents(fileName);
+            return Template.Parse(content);
+        }
+    }
+
+    public record EmbeddedResourceProvider(System.Reflection.Assembly assembly, string root) : IFileProvider
+    {
+        EmbeddedFileProvider embeddedFileProvider = new EmbeddedFileProvider(assembly, root);
+        ImmutableDictionary<string, Template> templateCache = ImmutableDictionary<string, Template>.Empty;
+
+        public string getFileContents(string fileName)
+        {
+            var fileInfo = embeddedFileProvider.GetFileInfo(fileName);
+            using var stream = fileInfo.CreateReadStream();
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+        public Template getTemplate(string fileName)
+        {
+            if (templateCache.TryGetValue(fileName, out var template))
+            {
+                return template;
+            }
+            else
+            {
+                var content = getFileContents(fileName);
+                template = Template.Parse(content);
+                templateCache = templateCache.Add(fileName, template);
+                return template;
+            }
+        }
+    }
+
+    public static class FileProviderExtensions {
+        public static IServiceCollection ConfigureFileProvider(this IServiceCollection services) {
+            #if DEBUG
+                services.AddSingleton<IFileProvider>(new LocalFileProvider("www"));
+            #else
+                services.AddSingleton<IFileProvider>(new embeddedFileProvider(
+                    System.Reflection.Assembly.GetExecutingAssembly(),
+                    "allowanced/www")
+            #endif
+            return services;
+        }
+    }
+}
--- allowanced/Program.cs
+++ allowanced/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Scriban;
+
+namespace allowanced
+{
+  class Program
+  {
+    public static IResult Html(string html)
+    {
+      return Results.Content(html, "text/html");
+    }
+
+    public static string HelloRoute2() => "hello world";
+
+    public static async Task<IResult> nameRoute(HttpContext ctx, IFileProvider fileProvider)
+    {
+      var template = fileProvider.getTemplate("name.html");
+      var result = await template.RenderAsync(new { name = "JOE" });
+      Console.WriteLine(result);
+      return Html(result);
+    }
+
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+      builder.Services.ConfigureFileProvider();
+      builder.Services.AddAuthentication("cookie").AddCookie("cookie");
+
+      var app = builder.Build();
+      app.MapGet("/", HelloRoute2);
+      app.MapGet("/name", nameRoute);
+      app.Run();
+    }
+  }
+}
